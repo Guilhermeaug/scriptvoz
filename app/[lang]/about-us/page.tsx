52053e8886424680884e599c5a8237ee4104269e
Guilhermeaug <guilhermeaugustodeoliveira66@gmail.com>
2024-05-18 18:14:30 -0300

    ..

--- app/[lang]/about-us/page.tsx
+++ app/[lang]/about-us/page.tsx
@@ -1,7 +1,11 @@
 import BlocksRendererClient from '@/components/BlocksRendererClient';
 import InformationHeader from '@/components/InformationHeader';
 import { getPageData } from '@/lib/page_data';
+import Guilherme from '@/public/people/GuilhermeAugusto.jpg';
 import { AboutUsPage as AboutUsPageAttributes } from '@/types/page_types';
+import Image from 'next/image';
+
+import styles from './styles.module.css';
 
 export default async function AboutUsPage({
   params: { lang },
@@ -16,40 +20,55 @@ export default async function AboutUsPage({
   });
 
   return (
-    <div className='container mx-auto mt-3 max-w-screen-md space-y-6 p-3 md:p-8'>
-      <h1 className='text-4xl font-bold text-accent'>Sobre nós</h1>
-      <section className='text-xl'>
+    <div className='container mx-auto mt-6 space-y-4 p-4'>
+      <h1 className='text-4xl font-bold'>{pageAttributes.what_is}</h1>
+      <div>
+        <BlocksRendererClient content={pageAttributes.text} />
+      </div>
+      <div>
+        <Member />
+        <div className='prose prose-stone text-justify md:prose-xl md:grid md:grid-cols-2 md:gap-x-12 md:gap-y-1'>
+          <div>
+            <InformationHeader title={pageAttributes.authors} />
+            <p>Lorena Luiza Costa Rosa Nogueira</p>
+            <p>Sandro Renato Dias</p>
+            <p>Anna Alice Figueirêdo de Almeida</p>
+            <p>Renata Rangel Azevedo</p>
+            <p>Ana Cristina Côrtes Gama </p>
+          </div>
+          <div>
+            <InformationHeader title={pageAttributes.developer} />
+            <p>Guilherme Augusto de Oliveira</p>
+          </div>
+          <div className='col-span-2'>
+            <InformationHeader title={pageAttributes.maintainer} />
+            <p>Vinicius Ferreira Pinheiro</p>
+          </div>
+        </div>
+        <hr className='my-4 border-t border-t-primary py-4 text-primary' />
         <div className='prose prose-stone text-justify md:prose-xl'>
-          <InformationHeader title='O que é o ScriptVoz?' />
-          <BlocksRendererClient content={pageAttributes.text} />
+          <InformationHeader title={pageAttributes.contributors} />
+          <p>Gleiston Assis Filho (CEFET)</p>
+          <p>Júlia de Oliveira Alves (UFMG)</p>
         </div>
-        <div>
-          <div className='prose prose-stone text-justify md:prose-xl md:grid md:grid-cols-2 md:gap-x-12 md:gap-y-1'>
-            <div>
-              <InformationHeader title={pageAttributes.authors} />
-              <p>Lorena Luiza Costa Rosa Nogueira</p>
-              <p>Sandro Renato Dias</p>
-              <p>Anna Alice Figueirêdo de Almeida</p>
-              <p>Renata Rangel Azevedo</p>
-              <p>Ana Cristina Côrtes Gama </p>
-            </div>
-            <div>
-              <InformationHeader title={pageAttributes.developer} />
-              <p>Guilherme Augusto de Oliveira</p>
-            </div>
-            <div className='col-span-2'>
-              <InformationHeader title={pageAttributes.maintainer} />
-              <p>Vinicius Ferreira Pinheiro</p>
-            </div>
-          </div>
-          <hr className='my-4 border-t border-t-primary py-4 text-primary' />
-          <div className='prose prose-stone text-justify md:prose-xl'>
-            <InformationHeader title={pageAttributes.contributors} />
-            <p>Gleiston Assis Filho (CEFET)</p>
-            <p>Júlia de Oliveira Alves (UFMG)</p>
-          </div>
+      </div>
+    </div>
+  );
+}
+
+function Member() {
+  return (
+    <div className=''>
+      <div className={styles.container}>
+        <div className={styles.image}>
+          <Image
+            className='block h-auto w-96 max-w-full rounded-lg shadow-md'
+            src={Guilherme}
+            alt='Picture of Guilherme Augusto de Oliveira'
+          />
         </div>
-      </section>
+        <div className={styles.text}>Guilherme Augusto de Oliveira</div>
+      </div>
     </div>
   );
 }
