a92fce31b0543eb88d1be711e46eeff0304b43d3
Guilherme Augusto <guilherme.oliveira@hotmart.com>
2024-06-12 10:38:08 -0300

    chore: Update Clarity analytics script strategy in layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 
 import { NextAuthProvider } from '@/contexts/SessionProvider';
 import { i18n } from '@/i18n-config';
+import MicrosoftClarity from '@/scripts/clarity';
 import { Nunito_Sans } from 'next/font/google';
-import Script from 'next/script';
 import { Toaster } from 'sonner';
 
 export const metadata = {
@@ -30,23 +30,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang={lang} className={nunito.className}>
-      <head>
-        <Script id="clarity-script" strategy="beforeInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "mqslegzqke");
-          `}
-        </Script>
-      </head>
       <body className='flex min-h-screen flex-col'>
         <NextAuthProvider>
           <Navbar lang={lang} />
           <main className='relative flex-grow'>{children}</main>
           <Toaster />
         </NextAuthProvider>
+        <MicrosoftClarity />
       </body>
     </html>
   );
