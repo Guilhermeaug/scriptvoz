65db0d1444ab069840b6c0f61368cd722365972b
Guilherme Augusto <guilherme.oliveira@hotmart.com>
2024-06-12 09:53:42 -0300

    chore: Add Clarity analytics script to layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { NextAuthProvider } from '@/contexts/SessionProvider';
 import { i18n } from '@/i18n-config';
 import { Nunito_Sans } from 'next/font/google';
+import Script from 'next/script';
 import { Toaster } from 'sonner';
 
 export const metadata = {
@@ -29,6 +30,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang={lang} className={nunito.className}>
+      <head>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "mqslegzqke");
+          `}
+        </Script>
+      </head>
       <body className='flex min-h-screen flex-col'>
         <NextAuthProvider>
           <Navbar lang={lang} />
