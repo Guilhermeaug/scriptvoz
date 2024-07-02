a3f0141c616ffdca87d36b3f0fe644b0fddd4f5b
Guilherme Augusto <guilherme.oliveira@hotmart.com>
2024-07-02 18:54:40 -0300

    Update layout.tsx to include authentication session in RootLayout

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 
 import { NextAuthProvider } from '@/contexts/SessionProvider';
 import { i18n } from '@/i18n-config';
+import { useAuth } from '@/lib/auth';
 import MicrosoftClarity from '@/scripts/clarity';
 import { Nunito_Sans } from 'next/font/google';
 import { Toaster } from 'sonner';
@@ -21,17 +22,19 @@ export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
   params: { lang },
 }: {
   children: React.ReactNode;
   params: { lang: string };
 }) {
+  const session = await useAuth();
+
   return (
     <html lang={lang} className={nunito.className}>
       <body className='flex min-h-screen flex-col'>
-        <NextAuthProvider>
+        <NextAuthProvider session={session}>
           <Navbar lang={lang} />
           <main className='relative flex-grow'>{children}</main>
           <Toaster />
--- contexts/SessionProvider.tsx
+++ contexts/SessionProvider.tsx
@@ -1,12 +1,14 @@
 'use client';
 
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 
 type Props = {
+  session: Session | null;
   children?: ReactNode;
 };
 
-export const NextAuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+export const NextAuthProvider = ({ session, children }: Props) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
